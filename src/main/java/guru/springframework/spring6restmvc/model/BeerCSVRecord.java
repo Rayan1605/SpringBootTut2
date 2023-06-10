3f2aacd4cbc87a158635f0d1a49d822a55468be0
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-06-10 17:20:57 +0100

    Tut2

--- src/main/java/guru/springframework/spring6restmvc/model/BeerCSVRecord.java
+++ src/main/java/guru/springframework/spring6restmvc/model/BeerCSVRecord.java
@@ -9,4 +9,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class BeerCSVRecord {
     private Integer row;
+    private Integer count;
+    private String abv;
+    private String ibu;
+    private Integer id;
+    private String beer;
+    private String style;
+    private Integer breweryId;
+    private Float ounces;
+    private String style2;
+    private String count_y;
+    private String city;
+    private String state;
+    private String label;
 }
