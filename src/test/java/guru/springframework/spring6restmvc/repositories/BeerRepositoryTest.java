1d62efd94552b58123f45ac4e41e21cadb42201f
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-06-16 15:39:55 +0100

    Tut2

--- src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -1,10 +1,13 @@
 package guru.springframework.spring6restmvc.repositories;
 
+import guru.springframework.spring6restmvc.bootstrap.BootstrapData;
 import guru.springframework.spring6restmvc.entities.Beer;
+import guru.springframework.spring6restmvc.model.BeerCSVRecord;
 import guru.springframework.spring6restmvc.model.BeerStyle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -12,6 +15,9 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@Import({BootstrapData.class, BeerCSVRecord.class}) // this is to import the bootstrap data class
+    // so that we can use it in our test and also the BeerCSVRecord class so that we can use it in our test
+    // it will contain the data that we will use in our test
 class BeerRepositoryTest {
 
     @Autowired
@@ -25,6 +31,8 @@ class BeerRepositoryTest {
         //also that findAllByBeerNameIsLikeIgnoreCase is a jpa method that is created by spring data jpa
         //we configured that in our BeerRepository interface
         List<Beer> list = beerRepository.findAllByBeerNameIsLikeIgnoreCase("%IPA%");
+
+         assertThat(list.size()).isEqualTo(30);
     }
     @Test
     void testSaveBeer() {
