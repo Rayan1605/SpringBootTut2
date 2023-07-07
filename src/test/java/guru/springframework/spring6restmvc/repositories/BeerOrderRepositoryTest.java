46577cf30a41ff60acd1ad7bda2109d547e81942
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-07 14:54:31 +0100

    Tut2

--- src/test/java/guru/springframework/spring6restmvc/repositories/BeerOrderRepositoryTest.java
+++ src/test/java/guru/springframework/spring6restmvc/repositories/BeerOrderRepositoryTest.java
@@ -1,16 +1,32 @@
 package guru.springframework.spring6restmvc.repositories;
 
+import guru.springframework.spring6restmvc.entities.Beer;
+import guru.springframework.spring6restmvc.entities.Customer;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
-@DataJpaTest // this will bring up an embedded database and configure Spring Data JPA for us
+@SpringBootTest
 class BeerOrderRepositoryTest {
  @Autowired // this will inject the BeerOrderRepository for us
     BeerOrderRepository  beerOrderRepository;
 @Autowired// this will inject the CustomerRepository for us
  CustomerRepository customerRepository;
+
+@Autowired
+    BeerRepository beerRepository;
+
+Customer testCustomer;
+Beer testBeer;
+
+@BeforeEach
+void setup(){
+testCustomer = customerRepository.findAll().get(0);
+testBeer = beerRepository.findAll().get(0);
+}
 
     @Test
     void testBeerOrder(){
