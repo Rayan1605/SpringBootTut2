51e34f30d12bfb4bcabbcaa842daeda6618478e6
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-07-07 15:43:50 +0100

    Tut2

--- src/main/java/guru/springframework/spring6restmvc/entities/BeerOrder.java
+++ src/main/java/guru/springframework/spring6restmvc/entities/BeerOrder.java
@@ -25,7 +25,9 @@ public class BeerOrder {
         this.created_date = created_date;
         this.lastModifiedDate = lastModifiedDate;
         this.customerRef = customerRef;
-        this.SetCustomer(customer);
+        this.SetCustomer(customer); // so we can ovveride because we are calling the builder method
+        // it will use all args constructor so we override to make sure it uses our
+        // setCustomer method instead of the constructor
         this.beerOrderLineSet = beerOrderLineSet;
     }
 
